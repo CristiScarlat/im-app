e5e18c5ba0af75e0b64ca25811c2cb89cfe108a8
Adriana Roznovan <adiza@192-168-0-178.rdsnet.ro>
2023-03-17 22:49:40 +0200

    Implement home page

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,8 @@
 import { Inter } from 'next/font/google'
+import LandingPage from './home'
 import styles from '@/styles/Home.module.css'
 
 
 export default function Home() {
-  return (
-    <>
-      <h1 className="text-green-500 text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    </>
-  )
+  return <LandingPage />
 }
