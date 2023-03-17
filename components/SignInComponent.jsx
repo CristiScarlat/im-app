17cd1bda3b34f71be5507b2a5b79fba122940143
stanescu777 <alexandru.stanescu97@gmail.com>
2023-03-17 23:33:40 +0200

    integrated userbase sign

--- components/SignInComponent.jsx
+++ components/SignInComponent.jsx
@@ -7,6 +7,7 @@ function SignInComponent() {
       <h2 className="text-3xl font-bold text-green-500 mb-2">
         Sign in to Account
       </h2>
+
       <SocialComponents />
 
       <p className="text-gray-300">or use your email account</p>
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,19 @@
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
-
+import { useAuthState } from "@/components/context/auth";
 
 export default function Home() {
+  const { isAuthenticated, user } = useAuthState();
+
+  console.log(user);
+
   return (
     <>
       <h1 className="text-green-500 text-3xl font-bold underline">
-        Hello world!
+        {isAuthenticated
+          ? "Hello Guest"
+          : user && user.username
+          ? `Hello ${user.username}`
+          : "Hello"}
       </h1>
     </>
-  )
+  );
 }
